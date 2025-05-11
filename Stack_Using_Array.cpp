72400a54ab853826ac1d348e09d62eebe161dbb6
yash paunikar <159148642+yashpaunikar2000@users.noreply.github.com>
2025-05-11 18:51:07 +0530

    Stack_Using_Array.cpp

---
+++ Stack_Using_Array.cpp
@@ -0,0 +1,32 @@
+/*
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+*/
+
+
+
+void MyStack ::push(int x) {
+    if(top==999){
+        return ;
+    }
+    top++;
+    arr[top]=x;
+} 
+
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+    if(top==-1){
+        return -1;
+    }
+    int x=arr[top];
+    top--;
+    return x;
+}
